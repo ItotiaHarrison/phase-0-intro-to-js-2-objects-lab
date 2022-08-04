ae360de18b29ce122d2ba1cd364e6553927e3200
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-04 23:48:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    name: "Harrison",
+    streetAddress: "Westlands",
+}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+        return {...employee, [key]: value};
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+};
+
+
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
+
+
